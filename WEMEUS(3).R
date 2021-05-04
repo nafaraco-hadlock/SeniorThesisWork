737690916b633419d745e69752a4b5790f4160ed
nafaraco-hadlock <81657026+nafaraco-hadlock@users.noreply.github.com>
2021-05-04 18:40:41 -0400

    Add files via upload
    Started to rasterize plant hardiness zone

--- WEMEUS(3).R
+++ WEMEUS(3).R
@@ -276,10 +276,43 @@ model.sel (lme1, lme2, lme3, lme4, lme5)
 summary(lme1)
 summary(lme2)
 
+#Rasterize Plant Hardiness Zone
 
+PHZ= readOGR(dsn=path.expand('\\rschfs1x\ userrs\ K-Q\ naf42_RS\ Desktop\ Senior Thesis Work'),layer='phm_us_shp.shp')
+s <- shapefile("\\rschfs1x\ userrs\ K-Q\ naf42_RS\ Desktop\ Senior Thesis Work\ phm_us_shp.shp")
+shp2raster <- function(shp, mask.raster, label, value, transform = FALSE, proj.from = NA,
+                       proj.to = NA, map = TRUE) {
+  require(raster, rgdal)
+  
+  # use transform==TRUE if the polygon is not in the same coordinate system as
+  # the output raster, setting proj.from & proj.to to the appropriate
+  # projections
+  if (transform == TRUE) {
+    proj4string(shp) <- proj.from
+    shp <- spTransform(shp, proj.to)
+  }
+  
+  # convert the shapefile to a raster based on a standardised background
+  # raster
+  r <- rasterize(shp, mask.raster)
+  # set the cells associated with the shapfile to the specified value
+  r[!is.na(r)] <- value
+  # merge the new raster with the mask raster and export to the working
+  # directory as a tif file
+  r <- mask(merge(r, mask.raster), mask.raster, filename = label, format = "GTiff",
+            overwrite = T)
+  
+  # plot map of new raster
+  if (map == TRUE) {
+    plot(r, main = label, axes = F, box = F)
+  }
+  
+  names(r) <- label
+  return(r)
+}
+RasterPHZ = shp2raster('phm_us_shp.shp',raster(xmn=-125,xmx=-66,ymn=23,ymx=50),transform=TRUE, map=TRUE, proj.from="+proj=longlat +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +no_defs ",proj.to="+proj=longlat +datum=WGS84 +no_defs ")
 
-
-
+?readOGR
 
 
 #Plot Population Trend as a function of Latitude
